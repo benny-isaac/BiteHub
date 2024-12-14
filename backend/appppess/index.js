f78823cd9b4606e45e5fc9c3a46d4db8652183c1
deuce-szn <bryananaafi08@gmail.com>
2024-12-14 09:40:45 +0000

    updated src to api

---
+++ backend/appppess/index.js
@@ -0,0 +1,15 @@
+// import app from "./src/server.js";
+// export default app;
+
+
+
+// import express from 'express';
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// app.listen(5000, () => console.log("Server ready on port 5000."));
+
+// export default app;
